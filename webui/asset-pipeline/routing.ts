3a4f2c9743cda7c29acf039ef3d31668c1085c1d
$NAME <$EMAIL>
2021-11-17 12:35:03 +0000

    Highlight menu correctly.

--- webui/asset-pipeline/routing.ts
+++ webui/asset-pipeline/routing.ts
@@ -4,19 +4,23 @@ const route = rlite(notFound, {
     // Default route
     '': function () {
         showSection('vaults')
+        document.getElementById('vaults-link').classList.add('selected')
     },
 
     'vault-details': function () {
         showSection('vault-details')
+        document.getElementById('vaults-link').classList.add('selected')
     },
 
     'audit': function () {
         showSection('audit')
+        document.getElementById('audit-link').classList.add('selected')
     },
 
     // #inbox
     'users': function () {
         showSection('users')
+        document.getElementById('users-link').classList.add('selected')
     }
 });
 
@@ -43,5 +47,9 @@ function showSection(name) {
         sections[i].style.display = 'none'
     }
     document.getElementById(name).style.display = null
+
+    document.getElementById('vaults-link').classList.remove('selected')
+    document.getElementById('users-link').classList.remove('selected')
+    document.getElementById('audit-link').classList.remove('selected')
     return false
 }
