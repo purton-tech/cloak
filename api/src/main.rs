439a3c744f6945160863db5a2d835fd8816558c5
Ian Purton <ian.purton@gmail.com>
2021-12-04 13:21:29 +0000

    Starter gRPC Server.

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use hello_world::greeter_server::GreeterServer;
+use tonic::transport::Server;
+mod server;
+
+pub mod hello_world {
+    tonic::include_proto!("helloworld"); // The string specified here must match the proto package name
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse()?;
+    let greeter = server::MyGreeter::default();
+
+    Server::builder()
+        .add_service(GreeterServer::new(greeter))
+        .serve(addr)
+        .await?;
+
+    Ok(())
 }
---
+++ api/src/server.rs
@@ -0,0 +1,23 @@
+use tonic::{Request, Response, Status};
+
+use crate::hello_world::greeter_server::Greeter;
+use crate::hello_world::{HelloReply, HelloRequest};
+
+#[derive(Debug, Default)]
+pub struct MyGreeter {}
+
+#[tonic::async_trait]
+impl Greeter for MyGreeter {
+    async fn say_hello(
+        &self,
+        request: Request<HelloRequest>,
+    ) -> Result<Response<HelloReply>, Status> {
+        println!("Got a request: {:?}", request);
+
+        let reply = HelloReply {
+            message: format!("Hello {}!", request.into_inner().name),
+        };
+
+        Ok(Response::new(reply))
+    }
+}
