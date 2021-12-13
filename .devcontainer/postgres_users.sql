deea5404326606d33a0865f56c1773ff935d0a39
$NAME <$EMAIL>
2021-12-13 08:22:14 +0000

    Call API from CLI.

--- .devcontainer/postgres_users.sql
+++
@@ -1,7 +0,0 @@
--- These users will need to be created in production with the same names
--- All the permissions will need to be setup with the migrations.
-CREATE USER keyvault_app LOGIN ENCRYPTED PASSWORD 'testpassword';
-CREATE USER keyvault_auth LOGIN ENCRYPTED PASSWORD 'testpassword';
-CREATE USER keyvault_readonly LOGIN ENCRYPTED PASSWORD 'testpassword';
--- The migrations users
-CREATE USER keyvault_migrations CREATEDB CREATEROLE LOGIN ENCRYPTED PASSWORD 'testpassword';
