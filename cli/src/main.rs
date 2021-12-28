fc9d11753df1e81f4ba4520cc1a4124d7e3af3c6
Ian Purton <ian.purton@gmail.com>
2021-12-28 15:01:49 +0000

    Call a subcommand and pass in env.

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -2,6 +2,61 @@ pub mod vault {
     tonic::include_proto!("vault");
 }
 
+use clap::{AppSettings, Parser, Subcommand};
+use std::collections::HashMap;
+use std::env;
+use std::ffi::OsString;
+use std::path::PathBuf;
+use std::process::{Command, Stdio};
+
+/// A fictional versioning CLI
+#[derive(Parser)]
+#[clap(name = "git")]
+#[clap(about = "A fictional versioning CLI")]
+struct Cli {
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// pushes things
+    #[clap(setting(AppSettings::ArgRequiredElseHelp))]
+    Init {
+        /// The remote to target
+        remote: String,
+    },
+    /// adds things
+    #[clap(setting(AppSettings::ArgRequiredElseHelp))]
+    Read {
+        /// Stuff to add
+        #[clap(required = true, parse(from_os_str))]
+        path: Vec<PathBuf>,
+    },
+    /// adds things
+    #[clap(setting(AppSettings::ArgRequiredElseHelp))]
+    Write {
+        /// Stuff to add
+        #[clap(required = true, parse(from_os_str))]
+        path: Vec<PathBuf>,
+    },
+    /// pushes things
+    #[clap(setting(AppSettings::ArgRequiredElseHelp))]
+    Generate {
+        /// The remote to target
+        remote: String,
+    },
+    /// pushes things
+    #[clap(setting(AppSettings::ArgRequiredElseHelp))]
+    Ls {
+        /// The remote to target
+        remote: String,
+    },
+    /// Runs a program and passes secret environment variables to it
+    #[clap(external_subcommand)]
+    Run(Vec<OsString>),
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let client = vault::vault_client::Vault::new(String::from("https://keyvault.authn.tech"));
@@ -10,6 +65,40 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let response = client.list_vaults(vault::ListVaultsRequest {}).await?;
 
     println!("RESPONSE={:?}", response);
+
+    let args = Cli::parse();
+
+    match &args.command {
+        Commands::Init { remote } => {
+            println!("Cloning {}", remote);
+        }
+        Commands::Read { path } => {
+            println!("Adding {:?}", path);
+        }
+        Commands::Write { path } => {
+            println!("Adding {:?}", path);
+        }
+        Commands::Generate { remote } => {
+            println!("Adding {:?}", remote);
+        }
+        Commands::Ls { remote } => {
+            println!("Adding {:?}", remote);
+        }
+        Commands::Run(args) => {
+            println!("Calling out to {:?} with {:?}", &args[0], &args[1..]);
+            let filtered_env: HashMap<String, String> = env::vars()
+                .filter(|&(ref k, _)| k == "TERM" || k == "TZ" || k == "LANG" || k == "PATH")
+                .collect();
+
+            Command::new("printenv")
+                .stdin(Stdio::null())
+                .stdout(Stdio::inherit())
+                .env_clear()
+                .envs(&filtered_env)
+                .spawn()
+                .expect("printenv failed to start");
+        }
+    }
 
     Ok(())
 }
