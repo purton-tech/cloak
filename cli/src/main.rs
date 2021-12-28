7c2119ce0e761f37e69ddd57ba51333fe4e3e955
Ian Purton <ian.purton@gmail.com>
2021-12-28 10:52:45 +0000

    Works over gRPC.

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -4,8 +4,8 @@ pub mod vault {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    //let mut client = vault_client::VaultClient::connect("https://keyvault.authn.tech").await?;
-    let client = vault::vault_client::Vault::new(String::from("http://envoy:7100"));
+    let client = vault::vault_client::Vault::new(String::from("https://keyvault.authn.tech"));
+    //let client = vault::vault_client::Vault::new(String::from("http://envoy:7100"));
 
     let response = client.list_vaults(vault::ListVaultsRequest {}).await?;
 
