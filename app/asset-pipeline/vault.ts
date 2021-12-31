9d7018a3cf516d6d9aeb787e197e9c141c8e508b
Ian Purton <ian.purton@gmail.com>
2021-12-31 09:36:37 +0000

    Create a key.

--- app/asset-pipeline/vault.ts
+++ app/asset-pipeline/vault.ts
@@ -1,13 +1,49 @@
 import { openDB } from 'idb';
+
+const AES_OPTIONS = {
+    name: "AES-GCM",
+    length: 256
+};
+
 export class Vault {
 
-    public static async encrypt(bytesToEncrypt: ByteData): Promise<ByteData> {
+    public static async newWrappedAesKey(): Promise<Cipher> {
 
         const db = await this.openIndexedDB()
-        const ecdsaKey = await db.get('keyval', 'unprotected_symmetric_key') as CryptoKey
+        const aesKey = await db.get('keyval', 'unprotected_symmetric_key') as CryptoKey
         db.close()
 
-        return bytesToEncrypt
+        const newAesKey = await self.crypto.subtle.generateKey(
+            AES_OPTIONS,
+            true,
+            ['decrypt', 'encrypt'])
+        const symKeyData = new ByteData(await self.crypto.subtle.exportKey('raw', newAesKey))
+        const protectedSymKey = await this.aesEncrypt(symKeyData.arr, aesKey);
+
+        return protectedSymKey
+    }
+
+    private static async aesEncrypt(data: Uint8Array, key: CryptoKey): Promise<Cipher> {
+    
+        const encOptions = {
+            name: 'AES-GCM',
+            iv: new Uint8Array(16)
+        };
+        self.crypto.getRandomValues(encOptions.iv);
+        const ivData = new ByteData(encOptions.iv.buffer);
+        const cipher = new ByteData(await self.crypto.subtle.encrypt(encOptions, key, data))
+    
+        return new Cipher(ivData, cipher)
+    }
+    
+    private static async aesDecrypt(cipher: Cipher, key: CryptoKey) {
+    
+        const decOptions = {
+            name: 'AES-GCM',
+            iv: cipher.iv.arr.buffer
+        };
+    
+        return new ByteData(await self.crypto.subtle.decrypt(decOptions, key, cipher.ct.arr.buffer));
     }
 
     private static async openIndexedDB() {
@@ -17,7 +53,48 @@ export class Vault {
             },
         });
     }
+
+    // Copied from https://stackoverflow.com/questions/39554165/ecdsa-signatures-between-node-js-and-webcrypto-appear-to-be-incompatible
+    // It generated hex, we could write a more efficient one that jsut works with bytes.
+    private static toDER(signature: ByteData): ByteData {
+    
+        // Extract r & s and format it in ASN1 format.
+        var signHex = Array.prototype.map.call(signature.arr, function (x) { return ('00' + x.toString(16)).slice(-2); }).join(''),
+            r = signHex.substring(0, 96),
+            s = signHex.substring(96),
+            rPre = true,
+            sPre = true;
+    
+        while (r.indexOf('00') === 0) {
+            r = r.substring(2);
+            rPre = false;
+        }
+    
+        if (rPre && parseInt(r.substring(0, 2), 16) > 127) {
+            r = '00' + r;
+        }
+    
+        while (s.indexOf('00') === 0) {
+            s = s.substring(2);
+            sPre = false;
+        }
+    
+        if (sPre && parseInt(s.substring(0, 2), 16) > 127) {
+            s = '00' + s;
+        }
+    
+        const payload = '02' + this.lengthOfHex(r) + r + '02' + this.lengthOfHex(s) + s
+        const der = '30' + this.lengthOfHex(payload) + payload
+    
+        return ByteData.fromHex(der)
+    }
+
+    // Auxs
+    private static lengthOfHex(hex) {
+        return ('00' + (hex.length / 2).toString(16)).slice(-2).toString();
+    }
 }
+
 
 export class ByteData {
 
@@ -69,5 +146,31 @@ export class ByteData {
         for (var bytes = [], c = 0; c < hex.length; c += 2)
             bytes.push(parseInt(hex.substr(c, 2), 16));
         return new this(bytes)
+    }
+}
+
+export class Cipher {
+
+    iv: ByteData
+    ct: ByteData
+    string: string
+
+    constructor(iv: ByteData, ct: ByteData) {
+        if (!arguments.length) {
+            this.iv = null;
+            this.ct = null;
+            this.string = null;
+            return;
+        }
+
+        this.iv = iv;
+        this.ct = ct;
+        this.string = iv.b64 + '|' + ct.b64;
+    }
+
+    static fromString(string: string) : Cipher {
+        const iv = ByteData.fromB64(string.split('|')[0])
+        const ct = ByteData.fromB64(string.split('|')[1])
+        return new this(iv, ct)
     }
 }
--- app/src/vaults/new_vault.ts
+++ app/src/vaults/new_vault.ts
@@ -1,12 +1,13 @@
 import SlDrawer from '@shoelace-style/shoelace/dist/components/drawer/drawer.js'
+import { Vault } from '../../asset-pipeline/vault'
 
 let newVaultButton = document.getElementById('new-vault')
 
-newVaultButton.addEventListener('click', event => {
+newVaultButton.addEventListener('click', async event => {
     let element = newVaultButton.previousElementSibling.firstChild
     if (element instanceof SlDrawer) {
         element.show()
 
-        document.getElementById('new-vault-key').innerText = "here"
+        document.getElementById('new-vault-key').innerText = (await Vault.newWrappedAesKey()).string
     }
 })
