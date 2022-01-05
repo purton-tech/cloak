64ba8ac97691eeaf2b73857b6e64627307eca13b
Ian Purton <ian.purton@gmail.com>
2022-01-05 10:50:28 +0000

    Comments.

--- app/src/service_accounts/view.ts
+++ app/src/service_accounts/view.ts
@@ -1,11 +1,16 @@
 import SlDrawer from '@shoelace-style/shoelace/dist/components/drawer/drawer.js'
 
+// The user wants to connect a service account to a vault
 const connectButton = document.getElementById('connect-to-vault')
+if(connectButton instanceof HTMLButtonElement) {
+    connectButton.addEventListener('click', async event => {
+        event.preventDefault()
+        alert('here')
+    })
+}
 
-// document.getElementById('service-account-row-{}').addEventListener('click',
-//() => document.getElementById('view-service-account-{}').show());
+// Configure all the drawers for each service account.
 document.querySelectorAll('[id^="service-account-row-"]').forEach((row) => {
-    console.log(row.id)
     row.addEventListener('click', () => {
         const drawer = document.getElementById('view-' + row.id)
         if(drawer instanceof SlDrawer) {
