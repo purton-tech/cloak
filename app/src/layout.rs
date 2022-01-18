2ea890415c465f58f39213788552516967a4d440
Ian Purton <ian.purton@gmail.com>
2022-01-18 07:22:59 +0000

    fix(security): Protobuf was generating code that required unsafe-inline for our CSP. This is fixed as part of the build.

--- app/src/layout.rs
+++ app/src/layout.rs
@@ -52,16 +52,12 @@ markup::define! {
 
                 link [ rel = "stylesheet", type="text/css" , href = crate::statics::get_index_css()] {}
 
-                // Only load resources from our server.
-                // style-src 'unsafe-inline' => Shoelace style insert styling into the dom.
-                // connect-src data: => Allow the inline svg for sl-drawer
-                // connect-src 'self' => So Web gRPC works
-                //
-                // We need unsafe-eval because iof this
-                // https://github.com/protocolbuffers/protobuf/issues/6770
+                // default-src 'self'           Only load resources from our server.
+                // style-src 'unsafe-inline'    Shoelace style inserts styling into the dom.
+                // connect-src data: =>         Allow the inline svg for sl-drawer
+                // connect-src 'self' =>        Allow connections back to the server so Web gRPC works
                 meta ["http-equiv"="Content-Security-Policy",
-                    content="default-src 'self'; script-src 'unsafe-eval';
-                        style-src 'unsafe-inline'; connect-src 'self' data:"] {}
+                    content="default-src 'self'; style-src 'unsafe-inline'; connect-src 'self' data:"] {}
 
             }
 
