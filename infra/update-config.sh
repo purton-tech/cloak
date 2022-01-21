3b3b026e05be9a54b881d2a873fe3a57d7e19c3e
Ian Purton <ian.purton@gmail.com>
2022-01-21 10:23:40 +0000

    fix(build): Tag docker images.

--- infra/update-config.sh
+++ infra/update-config.sh
@@ -1,9 +1,12 @@
-containers=( "purtontech/cloak-server:latest" "purtontech/cloak-db-migrations:latest" "purtontech/cloak-envoy:latest" "purtontech/cloak-website:latest")
+containers=( "purtontech/cloak-server" "purtontech/cloak-db-migrations" "purtontech/cloak-envoy" "purtontech/cloak-website")
 
 for i in "${containers[@]}"
 do
-   docker pull $i \
-    && CONFIG_NAME=$(echo $i | cut -c 11-) \
-    && HASH=$(docker inspect --format='{{index .RepoDigests 0}}' $i | cut -c 17-) \
-    && sed -i "/key$CONFIG_NAME/s/key$CONFIG_NAME.*$/key$CONFIG_NAME: $HASH/" Pulumi.prod.yaml 
+    docker pull $i 
+    CONFIG_NAME=$(echo $i | cut -c 12-) \
+    HASH=$(docker inspect --format='{{index .RepoDigests 0}}' $i )
+    HASH=$(echo $HASH | sed 's/^.*@//' )
+    echo "Name $CONFIG_NAME"
+    echo "Hash $HASH"
+    sed -i "s/keyvault:$CONFIG_NAME.*$/keyvault:$CONFIG_NAME: $HASH/" Pulumi.prod.yaml 
 done
