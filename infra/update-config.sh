bee241418fa33bc2ee7ec80377763417467e3128
Ian Purton <ian.purton@gmail.com>
2022-01-21 11:29:13 +0000

    fix(build): Capture image with tag.

--- infra/update-config.sh
+++ infra/update-config.sh
@@ -1,9 +1,9 @@
-containers=( "purtontech/cloak-server" "purtontech/cloak-db-migrations" "purtontech/cloak-envoy" "purtontech/cloak-website")
+containers=("purtontech/cloak-server" "purtontech/cloak-db-migrations" "purtontech/cloak-envoy" "purtontech/cloak-website")
 
 for i in "${containers[@]}"
 do
     docker pull $i 
-    CONFIG_NAME=$(echo $i | cut -c 12-) \
+    CONFIG_NAME=$(echo $i | cut -c 12-) 
     HASH=$(docker inspect --format='{{index .RepoDigests 0}}' $i )
     HASH=$(echo $HASH | sed 's/^.*@//' )
     echo "Name $CONFIG_NAME"
