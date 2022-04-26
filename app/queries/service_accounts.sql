e9943c456c66fd410e187810922aebf38c09531e
$NAME <$EMAIL>
2022-04-26 09:08:43 +0000

    All tests pass.

--- app/queries/service_accounts.sql
+++ app/queries/service_accounts.sql
@@ -63,7 +63,7 @@ ON
     v.id = sa.vault_id
 WHERE sa.ecdh_public_key = $1
 
---! get_dangerous(id) { id, vault_id?, account_name, vault_name, ecdh_public_key, encrypted_ecdh_private_key, updated_at, created_at }
+--! get_dangerous(id) { id, vault_id?, account_name, vault_name?, ecdh_public_key, encrypted_ecdh_private_key, updated_at, created_at }
 SELECT
     sa.id, sa.vault_id, sa.name, v.name as vault_name, 
     sa.ecdh_public_key, sa.encrypted_ecdh_private_key,
