d7a96defb69d47a26809a164d23421d3d287273c
Ian Purton <ian.purton@gmail.com>
2022-05-17 07:08:08 +0000

    Call it environment not folder.

--- app/src/secrets/new_secret.rs
+++ app/src/secrets/new_secret.rs
@@ -67,7 +67,7 @@ markup::define! {
                         label[for="secret"] { "Secret" }
                         textarea[rows="10", id="secret-value", type="text", autocomplete="off", required="", name="secret"] {}
 
-                        label[for="folder"] { "Folder" }
+                        label[for="folder"] { "Environment" }
                         select[name="environment_id"] {
                             @for environment in *environments {
                                 option[value=environment.id] {
