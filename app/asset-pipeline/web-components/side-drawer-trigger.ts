e8bf9649949496b54bd4b3417ec03d94b73fcfcc
Ian Purton <ian.purton@gmail.com>
2022-05-21 12:23:20 +0000

    Delete team mate.

--- app/asset-pipeline/web-components/side-drawer-trigger.ts
+++ app/asset-pipeline/web-components/side-drawer-trigger.ts
@@ -2,10 +2,14 @@ import { SideDrawer } from './side-drawer'
 
 // We can create a trigger to open drawers
 document.querySelectorAll('[data-drawer-target]').forEach(async (row) => {
+    console.log('here')
     // Detect when a user clicks a row
     row.addEventListener('click', (event) => {
+    console.log('clicked')
+
         event.stopImmediatePropagation()
         const drawer = document.getElementById(row.getAttribute('data-drawer-target'))
+        console.log(drawer)
         if (drawer instanceof SideDrawer) {
             drawer.open = true
         }
