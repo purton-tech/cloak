cb7ace9d9f0b35c49dd289435b00a430f7d94997
Ian Purton <ian.purton@gmail.com>
2023-01-12 15:02:24 +0000

    Make sure triggers fire with turbo

--- crates/asset-pipeline/web-components/side-drawer-trigger.ts
+++ crates/asset-pipeline/web-components/side-drawer-trigger.ts
@@ -1,22 +1,22 @@
 import { SideDrawer } from './side-drawer'
 
-
-document.addEventListener('readystatechange', () => {
-    if (document.readyState == 'complete') {
-        // We can create a trigger to open drawers
-        document.querySelectorAll('[data-drawer-target]').forEach(async (row) => {
-            // Detect when a user clicks a row
-            row.addEventListener('click', (event) => {
-        
-                event.stopImmediatePropagation()
-                const target = row.getAttribute('data-drawer-target')
-                if(target != null) {
-                    const drawer = document.getElementById(target)
-                    if (drawer instanceof SideDrawer) {
-                        drawer.open = true
-                    }
+const triggers = () => {
+    document.querySelectorAll('[data-drawer-target]').forEach(async (row) => {
+        // Detect when a user clicks a row
+        row.addEventListener('click', (event) => {
+            event.stopImmediatePropagation()
+            const attr = row.getAttribute('data-drawer-target');
+            if(attr) {
+                const drawer = document.getElementById(attr)
+                if(drawer) {
+                    drawer.setAttribute("open", "true")
                 }
-            })
+            } else {
+                console.log("side-drawer-trigger could not find data-drawer-target")
+            }
         })
-    }
-})
+    })
+}
+
+document.addEventListener("turbo:load", triggers)
+document.addEventListener("turbo:frame-load", triggers)
