0d7d908e38ea9f52d1eca9e65d6c006f6a6688fa
Ian Purton <ian.purton@gmail.com>
2023-01-18 08:46:29 +0000

    fix: Use pulumi yaml

--- .github/workflows/update-config.sh
+++ .github/workflows/update-config.sh
@@ -4,10 +4,12 @@ containers=("purtontech/cloak-server" "purtontech/cloak-db-migrations" "purtonte
 for i in "${containers[@]}"
 do
     docker pull $i 
-    CONFIG_NAME=$(echo $i | cut -c 12-) 
+    # i.e. turn "purtontech/cloak-server" into "server"
+    CONFIG_NAME=$(echo $i | cut -c 18-) 
     HASH=$(docker inspect --format='{{index .RepoDigests 0}}' $i )
     HASH=$(echo $HASH | sed 's/^.*@//' )
     echo "Name $CONFIG_NAME"
     echo "Hash $HASH"
-    sed -i "s/keyvault:$CONFIG_NAME.*$/keyvault:$CONFIG_NAME: $HASH/" Pulumi.prod.yaml 
+    # Update entries i.e. hash-trace-server: sha256:c677b52b14375c03eaede5ccdf4d40ccc9b7e9ef0157fd4b0f45b0e431f0c76d
+    sed -i "0,/hash-$CONFIG_NAME/{s/hash-$CONFIG_NAME.*$/hash-$CONFIG_NAME: $HASH/}" ../../Pulumi.yaml 
 done
