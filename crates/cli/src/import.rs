a254bf002199a242c683695a067243465f63431c
Ian Purton <ian.purton@gmail.com>
2023-03-24 16:09:33 +0000

    Get password

--- crates/cli/src/import.rs
+++ crates/cli/src/import.rs
@@ -1,12 +1,38 @@
 use super::keyring;
-use p256::pkcs8::EncodePrivateKey;
+use p256::{
+    pkcs8::{DecodePrivateKey, EncodePrivateKey},
+    SecretKey,
+};
 use rand_core::OsRng; // requires 'getrandom' feature
 
-pub async fn import(name: String, _key: String) {
-    let secret_key = p256::SecretKey::random(&mut OsRng);
+pub async fn import(name: String, key: String) {
+    // Pem format is 64 columns wide
+    let subs = key
+        .as_bytes()
+        .chunks(64)
+        .map(std::str::from_utf8)
+        .collect::<Result<Vec<&str>, _>>()
+        .unwrap();
+
+    let key = subs.join("\n");
+
+    let key = if key.contains("BEGIN PRIVATE") {
+        key
+    } else {
+        format!(
+            "-----BEGIN PRIVATE KEY-----
+{}
+-----END PRIVATE KEY-----",
+            key
+        )
+    };
+
+    let secret_key = SecretKey::from_pkcs8_pem(&key).expect("Problem Loading Key");
+
+    let password = rpassword::prompt_password("Your password: ").unwrap();
 
     let secret_key_serialized = secret_key
-        .to_pkcs8_encrypted_pem(&mut OsRng, "vja481xxx", Default::default())
+        .to_pkcs8_encrypted_pem(&mut OsRng, password, Default::default())
         .unwrap()
         .to_string();
 
